9bfa059cdee0a9d3adc8d9de27c4d48ad954019d
neutraltrykeep <neutraltrykeep@gmail.com>
2021-07-05 15:35:44 -0400

    msg added main()

--- TestEmp.java
+++ TestEmp.java
@@ -1,5 +1,8 @@
 public class MyEmpTest {
-    public void MyEmpTest () {
+    public MyEmpTest () {
         System.out.println("Hello My Emp Constr");
     }
+    public static void main(String[] args) {
+        System.out.println("Hi"); // Print the value of x
+      }    
 }
